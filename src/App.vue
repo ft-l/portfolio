f3a11ad0666a34d1ddc91ad31f415318caa39a01
William Skaggs <wskaggs3262@gmail.com>
2021-06-18 21:27:26 -0500

    Create a header bar with text

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <Header msg="Here is the text that will be in the header"></Header>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -7,10 +8,12 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Header from "@/components/Header";
 
 export default {
   name: 'App',
   components: {
+    Header,
     HelloWorld
   }
 }
@@ -23,6 +26,8 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  left: 0;
+  top: 0;
+  width: 100%;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="header-background">
+    <div class="header-text">
+      <h1>William Skaggs</h1>
+      <h1>Portfolio</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    msg: String
+  }
+}
+
+</script>
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Inconsolata&display=swap');
+.header-background {
+  background: #2c3e50;
+  width: 100%;
+  height: 100px;
+}
+
+.header-text {
+  color: white;
+  font-family: 'Inconsolata', monospace;
+  height: max-content;
+  margin-left: 20%;
+  margin-right: 20%;
+  text-align: left;
+}
+
+h1 {
+  font-weight: normal;
+}
+
+</style>
